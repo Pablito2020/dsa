932b73f96eb2a4b9d662db18e0b659065411e16c
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-11-21 09:37:18 +0100

    feat: add L parameter as a flag

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from cryptography.hazmat.primitives.asymmetric import dsa
-from cryptography.hazmat.primitives.asymmetric.dsa import DSAParameters, DSAParameterNumbers, DSAPrivateKey, DSAPrivateNumbers, DSAPublicKey
-from cryptography.hazmat.primitives.hashes import SHA256, Hash
-from cryptography.hazmat.primitives.asymmetric.utils import decode_dss_signature
+from argparse import ArgumentParser
 from dataclasses import dataclass
+
+from cryptography.hazmat.primitives.asymmetric import dsa
+from cryptography.hazmat.primitives.asymmetric.dsa import (DSAParameterNumbers,
+                                                           DSAParameters,
+                                                           DSAPrivateKey,
+                                                           DSAPrivateNumbers,
+                                                           DSAPublicKey)
+from cryptography.hazmat.primitives.asymmetric.utils import decode_dss_signature
+from cryptography.hazmat.primitives.hashes import SHA256, Hash
 
 
 @dataclass
 class DSA:
+    '''
+    Encapsulates all the parameters needed for the DSA algorithm.
+    '''
     L: int
     p: int
     q: int
@@ -16,8 +25,7 @@ class DSA:
     priv_key_obj: DSAPrivateKey
 
 
-def get_dsa_parameters() -> DSA:
-    L = 2048
+def get_dsa_parameters(L: int) -> DSA:
     values: DSAParameters = dsa.generate_parameters(key_size=L)
     numbers: DSAParameterNumbers = values.parameter_numbers()
     p = numbers.p
@@ -33,7 +41,9 @@ def get_dsa_parameters() -> DSA:
 
 def print_dsa_numbers(dsa_nums: DSA):
     print("Generated parameters for the DSA signature algorithm.")
-    print(f"\tL: {dsa_nums.L}\n\tp: {dsa_nums.p}\n\tq: {dsa_nums.q}\n\tg: {dsa_nums.g}\n\tx: {dsa_nums.x}\n\tpublic key (g^x modp): {dsa_nums.pub_k}")
+    print(
+        f"\tL: {dsa_nums.L}\n\tp: {dsa_nums.p}\n\tq: {dsa_nums.q}\n\tg: {dsa_nums.g}\n\tx: {dsa_nums.x}\n\tpublic key (g^x modp): {dsa_nums.pub_k}"
+    )
 
 
 def assert_h_equals_2(dsa_nums: DSA):
@@ -46,18 +56,30 @@ def assert_pub_key_is_g_pow_x_mod_p(dsa_nums: DSA):
     print("Asserted that the public key == g^x mod p!")
 
 
-def get_hash_of_message(message: str) -> int:
+def get_hash_of_message(message: str, length_bits: int) -> int:
+    """
+    returns the hash (function: SHA256) of the message and truncates the message
+    with the number of bits specified.
+    """
     digest = Hash(SHA256())
     digest.update(message.encode("utf-8"))
     message_bytes_hashed: bytes = digest.finalize()
-    return int.from_bytes(message_bytes_hashed, "big")
+    assert length_bits % 8 == 0
+    length_bytes = length_bits // 8
+    message_truncated = (
+        message_bytes_hashed
+        if length_bytes == 256
+        else message_bytes_hashed[:length_bytes]
+    )
+    return int.from_bytes(message_truncated, "big")
 
 
 def assert_v_equals_r(dsa_nums: DSA):
-    signature: bytes = dsa_nums.priv_key_obj.sign("Message".encode("utf-8"), SHA256())
+    dummy_message: str = "Message"
+    signature: bytes = dsa_nums.priv_key_obj.sign(dummy_message.encode("utf-8"), SHA256())
     r, s = decode_dss_signature(signature)
     w = pow(s, -1, dsa_nums.q)
-    message_hashed: int = get_hash_of_message("Message")
+    message_hashed: int = get_hash_of_message(dummy_message, dsa_nums.q.bit_length())
     a = (message_hashed * w) % dsa_nums.q
     b = (r * w) % dsa_nums.q
     v = pow(dsa_nums.g, a + (dsa_nums.x * b), dsa_nums.p) % dsa_nums.q
@@ -66,7 +88,16 @@ def assert_v_equals_r(dsa_nums: DSA):
 
 
 if __name__ == "__main__":
-    dsa_params: DSA = get_dsa_parameters()
+    # arg parser
+    parser = ArgumentParser(
+        prog="main.py", description="Create DSA parameters and assert the properties."
+    )
+    parser.add_argument("-l", "--number-bits-prime-number")
+    args = parser.parse_args()
+
+    # create and assert
+    L = int(args.number_bits_prime_number) if args.number_bits_prime_number else 2048
+    dsa_params: DSA = get_dsa_parameters(L)
     print_dsa_numbers(dsa_params)
     assert_h_equals_2(dsa_params)
     assert_pub_key_is_g_pow_x_mod_p(dsa_params)
